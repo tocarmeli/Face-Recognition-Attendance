423df1eea5d7358675d884abd13cb9c16fb1cc85
Tomer Carmeli <tocarmeli@gmail.com>
2024-01-07 21:59:35 -0500

    Initial commit

---
+++ add_faces.py
@@ -0,0 +1,74 @@
+# Check if face already exists in DIR
+# If it doesn't, then add face to DIR
+# If it does, ignore and move on to next face in img
+
+import cv2
+import numpy as np
+import os
+import shutil
+import face_recognition
+
+DIR = 'C:\\Users\\tocar\\Documents\\Code\\AttendanceSystem\\Face-Recognition-Attendance\\data'
+CURRENT_DIR = 'C:\\Users\\tocar\\Documents\\Code\\AttendanceSystem\\Face-Recognition-Attendance\\'
+dir_size = sum(os.path.getsize(f) for f in os.listdir(DIR) if os.path.isfile(f))
+print(dir_size)
+
+cam = cv2.VideoCapture(0)
+
+known_face_encodings = []
+
+# Loads all pictures from DIR
+for image in os.listdir(DIR):
+    img = face_recognition.load_image_file('data\\' + image)
+    known_face_encodings.append(face_recognition.face_encodings(img)[0])
+
+face_locations = []
+face_encodings = [] # Encodings of all the new faces, will be used to match with known_face_encodings
+names = []
+process_frame = True
+
+while True:
+    ret, frame = cam.read()
+
+    if process_frame:
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        rgb_small_frame = small_frame[:, :, ::-1]
+
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        names = []
+        for face_encoding in face_encodings:
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
+
+            # # If a match was found in known_face_encodings, just use the first one.
+            # if True in matches:
+            #     first_match_index = matches.index(True)
+            #     name = known_face_names[first_match_index]
+            if dir_size != 0:
+                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                # Checks to see if face exists in DIR
+                if matches[best_match_index]:
+                    print('User already added to database. Try again with a different user')
+                else:
+                    username = input('Enter username: ')
+                    cv2.imwrite(username + '.jpg', small_frame)
+            else:
+                username = input('Enter username: ')
+                cv2.imwrite(username + '.jpg', small_frame)
+
+            shutil.move(CURRENT_DIR + username + '.jpg', DIR + '\\' + username + '.jpg')
+                    
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+cam.release()
+cv2.destroyAllWindows()
---
+++ face_match.py
@@ -0,0 +1,79 @@
+import face_recognition
+import cv2
+import os
+import numpy as np
+
+# Directory as to where user photos are currently stored
+DIR = 'C:\\Users\\tocar\\Documents\\Code\\AttendanceSystem\\Face-Recognition-Attendance\\data'
+
+cam = cv2.VideoCapture(0)
+
+known_face_encodings = []
+known_names = [] # array of all user names
+
+# Loads all pictures from DIR
+for image in os.listdir(DIR):
+    name = input('Enter user name: ')
+    img = face_recognition.load_image_file('data\\' + image)
+    known_face_encodings.append(face_recognition.face_encodings(img)[0])
+    known_names.append(name)
+
+face_locations = []
+face_encodings = [] # Encodings of all the new faces, will be used to match with known_face_encodings
+names = []
+process_frame = True
+
+while True:
+    ret, frame = cam.read()
+
+    if process_frame:
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        rgb_small_frame = small_frame[:, :, ::-1]
+
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        names = []
+        for face_encoding in face_encodings:
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
+
+            # # If a match was found in known_face_encodings, just use the first one.
+            # if True in matches:
+            #     first_match_index = matches.index(True)
+            #     name = known_face_names[first_match_index]
+
+            # Or instead, use the known face with the smallest distance to the new face
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_names[best_match_index]
+
+            names.append(name)
+
+    process_frame = not process_frame
+    for (top, right, bottom, left), name in zip(face_locations, names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+cam.release()
+cv2.destroyAllWindows()
